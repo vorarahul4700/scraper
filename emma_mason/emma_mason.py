ddf3fc29457c85b71da32460a88ebf0e2dd9a7fa
Rahul Vora <rahul.vora.4182@gmail.com>
2026-09-01 15:58:56 +0530

    feat(emma_mason): apply working browser headers for Cloudflare compatibility

--- emma_mason/emma_mason.py
+++ emma_mason/emma_mason.py
@@ -80,6 +80,25 @@ class emmamasonScraper:
         }
 
         self.session = requests.Session()
+        self.headers = {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+            "accept-encoding": "gzip, deflate, br, zstd",
+            "accept-language": "en-US,en;q=0.9",
+            "cache-control": "no-cache",
+            "pragma": "no-cache",
+            "priority": "u=0, i",
+            "sec-ch-ua": '"Chromium";v="125", "Not.A/Brand";v="24", "Brave";v="125"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Windows"',
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "cross-site",
+            "sec-fetch-user": "?1",
+            "sec-gpc": "1",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+        }
+        self.session.headers.update(self.headers)
 
     # ============================================================
     #  Logger
